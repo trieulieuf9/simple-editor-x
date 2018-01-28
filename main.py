b4c0cc1a3018c5fc6b4595348681dbced7135cae
Trieu Nguyen <trieulieuf9@trieulieuf9s-MacBook-Pro.local>
2018-01-28 16:05:04 +0700

    big progress, can work properly and save file now. not yet open a file

--- main.py
+++ main.py
@@ -1,26 +1,42 @@
 from curses import *
-import curses.textpad
-
+import sys
 
 # add the word custom, so we will mistaken them with variable inside Curses library
 CUSTOM_KEY_ESCAPE = 27
 CUSTOM_KEY_BACKSPACE = 127
 CUSTOM_KEY_ENTER = 10
+CUSTOM_KEY_TAB = 9
 BORDER = 1
 
 def main(screen):
-    # setting up the screen
     use_default_colors()
     init_pair(BORDER, COLOR_BLUE, -1)
     width = 262
     height = 17
 
-    # begin your code here
-    cursor = Cursor(screen, width, height)
-
+    text = ""
     while 1:
+        text = startEditing(screen, width, height, text)
+        printQuitOptions(screen, width, height)
         char = screen.getch()
-        if char == KEY_F1 or char == CUSTOM_KEY_ESCAPE:
+        if char == CUSTOM_KEY_ENTER:
+            with open("Congratulation.txt", "w+") as file:
+                file.write(text)
+            break
+        elif char == KEY_F9:
+            break
+        else:
+            pass
+
+
+def startEditing(screen, width, height, text):
+    cursor = Cursor(screen, width, height, text)
+    while 1:
+        try:
+            char = screen.getch()
+        except KeyboardInterrupt: # quit properly, when user press Ctrl + C
+            sys.exit()
+        if char == KEY_F1:
             break
         elif char == KEY_RIGHT:
             cursor.moveRight()
@@ -36,16 +52,36 @@ def main(screen):
             cursor.delete()
         elif char == 10 or char == 13 or char == KEY_ENTER:
             cursor.newLine()
+        elif char == CUSTOM_KEY_TAB:
+            cursor.tab()
+        elif char == CUSTOM_KEY_ESCAPE:
+            char = screen.getch()  # get the key pressed after cmd
+            if char == KEY_LEFT:
+                cursor.moveToLeftMost()
+            elif char == KEY_RIGHT:
+                cursor.moveToRightMost()
+            else:  # in case char user press ESC
+                ungetch(char)
         else:
             cursor._writeString(str(char))
+    return cursor.getText()
 
-    endwin()
+
+def printQuitOptions(screen, width, height):
+    screen.clear()
+    y = int(height / 4)
+    # x = int(width / 4) # comment in, when run in real terminal
+    x = int(width / 8)
+    screen.addstr(y, x, "Quit and Save (ENTER)")
+    screen.addstr(y + 1, x, "Quit (F9)")
+    screen.addstr(y + 2, x, "Go Back (Any Key)")
+    screen.refresh()
 
 
 class Cursor:
 
-    def __init__(self, screen, screen_width, screen_height):
-        self.text = [ "" ]
+    def __init__(self, screen, screen_width, screen_height, text=""):
+        self.text = text.split("\n")
         self.count = 0
         self.x = 0
         self.y = 0
@@ -89,7 +125,7 @@ class Cursor:
                 self.text.insert(self.y + 1, line[self.x:])
             self._updateScreen()
             self.moveDown()
-            self._moveToLeftMost()
+            self.moveToLeftMost()
 
     def writeChar(self, char):
         self._updateXY()
@@ -100,12 +136,17 @@ class Cursor:
         self._updateScreen()
         self.moveRight()
 
+    def tab(self):
+        self.writeChar(32)
+        self.writeChar(32)
+        self.writeChar(32)
+
     def delete(self):
         self._updateXY()
         if len(self.text[self.y]) == 0 and self.y != 0:
             self.text.pop(self.y)
             self._updateScreen()
-            self._moveToRightMost()
+            self.moveToRightMost()
         elif len(self.text[self.y]) > 0 and len(self.text[self.y]) == self.x:
             self.text[self.y] = self.text[self.y][:-1]
             self._updateScreen()
@@ -120,6 +161,17 @@ class Cursor:
             self._updateScreen()
             self.moveUp()
             self._move(index, self.y - 1)
+
+    def moveToLeftMost(self):
+        self._updateXY()
+        self._move(0, self.y)
+
+    def moveToRightMost(self):
+        self._updateXY()
+        self._move(len(self.text[self.y]), self.y)
+
+    def getText(self):
+        return "\n".join(self.text)
 
     ### private function ###
     def _updateScreen(self):
@@ -137,14 +189,6 @@ class Cursor:
     def _move(self, x, y):
         self.screen.move(y, x)
 
-    def _moveToLeftMost(self):
-        self._updateXY()
-        self._move(0, self.y)
-
-    def _moveToRightMost(self):
-        self._updateXY()
-        self._move(len(self.text[self.y]), self.y)
-
     def _writeString(self, str):
         self.screen.addstr(str)
         self.moveRight()
@@ -161,3 +205,7 @@ class Cursor:
 
 if __name__== "__main__":
     wrapper(main)
+
+
+# todo
+# open existing file and edit
--- test.py
+++ test.py
@@ -5,9 +5,7 @@
 #
 # print(insert("f", "trieulieu9", len("trieulieuf9") - 2))
 # print(insert("f", "trieulieu9", 0))
-
-string = "trieulieuf9"
-
-print(string[:0] + string[1:])
+text = ""
+print(text.split("\n"))
 
 
